e565377225c5037de3fd012be3a7548a15d3b7cd
Raymond <raymond.yiu@gmail.com>
2023-01-17 09:41:19 +0000

    fist commit

---
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -0,0 +1,30 @@
+package com.techreturners.exercise001;
+
+import java.util.List;
+
+public class Exercise001 {
+    public String capitalizeWord(String word) {
+        // Add your code here
+        return "";
+    }
+
+    public String generateInitials(String firstName, String lastName) {
+        // Add your code here
+        return "";
+    }
+
+    public double addVat(double originalPrice, double vatRate) {
+        // Add your code here
+        return 0.0;
+    }
+
+    public String reverse(String sentence) {
+        // Add your code here
+        return "";
+    }
+
+    public int countLinuxUsers(List<User> users) {
+        // Add your code here
+        return 0;
+    }
+}
---
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -0,0 +1,15 @@
+package com.techreturners.exercise002;
+
+public class Exercise002 {
+
+    public boolean isFromManchester(/* Person person */) {
+        // Add your code here
+        return false;
+    }
+
+    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+        // Add your code here
+        return false;
+    }
+    
+}
