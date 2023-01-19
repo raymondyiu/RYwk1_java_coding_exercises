7b4398246d0cfe2672de8209218ae62ae33db0a0
Raymond <raymond.yiu@gmail.com>
2023-01-19 10:46:24 +0000

    exercise005 done

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,35 @@
 package com.techreturners.exercise005;
 
+import java.util.Arrays;
+
 public class Exercise005 {
+    private char[] AtoZ = { 'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z' };
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+
+        String defaultStr = "(.*)<XX>(.*)";
+        if (input.isEmpty()) {
+            return false;
+        }
+        String input2 = input.trim().toUpperCase();
+        if (input2.length() < AtoZ.length) {
+            return false;
+        }
+
+        int matchCount = 0;
+        for (int i = 0; i < AtoZ.length; i++) {
+            String regexStr = defaultStr.replace("<XX>", String.valueOf(AtoZ[i]));
+            if (!input2.matches(regexStr)) {
+                return false;
+            }
+            matchCount += 1;
+        }
+        return (matchCount == AtoZ.length);
+
     }
 
 }
